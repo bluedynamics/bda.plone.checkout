eed8909f9fe729bf27bb8b8624b2cd83c805d0b3
Robert Niederreiter <office@squarewave.at>
2012-08-07 13:54:47 +0200

    wip

--- src/bda/plone/checkout/browser/views.py
+++ src/bda/plone/checkout/browser/views.py
@@ -1,1 +1,38 @@
-#
+from yafowil.base import factory
+from yafowil.yaml import parse_from_YAML
+from yafowil.plone.form import Form
+from zope.i18nmessageid import MessageFactory
+from ..interfaces import ICheckoutDataAdapter
+
+
+_ = MessageFactory('bda.plone.checkout')
+fields_provider = list()
+
+
+class FieldsProvider(object):
+    fields_template = None
+    message_factory = _
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def extend(self, form):
+        fields = parse_from_YAML(self.fields_template, 
+                                 self, self.message_factory)
+        form[fields.name] = fields
+
+
+class CheckoutForm(Form):
+    
+    def prepare(self):
+        action = self.context.absolute_url() + '/@@checkout'
+        self.form = factory('#form', name='checkout', props={'action': action})
+        for provider in fields_provider:
+            provider.extend(self.form)
+        self.form['submit'] = factory('submit', props={
+            'label': _('save', 'Save'),
+            'action': 'save',
+            'handler': self.save})
+    
+    def save(self, widget, data):
+        ICheckoutDataAdapter(self.context).save(widget, data)
--- src/bda/plone/checkout/interfaces.py
+++ src/bda/plone/checkout/interfaces.py
@@ -4,3 +4,12 @@ from zope.interface import Interface
 class ICheckoutExtensionLayer(Interface):
     """Browser layer for bda.plone.checkout
     """
+
+
+class ICheckoutDataAdapter(Interface):
+    """Called for persisting checkout.
+    """
+    
+    def save(self, widget, data):
+        """Save checkout specific data.
+        """
