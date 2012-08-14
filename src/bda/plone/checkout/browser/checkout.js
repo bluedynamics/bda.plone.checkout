6834fcb2c7afffca4dcd1655d56046d3e8c6295d
Robert Niederreiter <office@squarewave.at>
2012-08-14 15:08:37 +0200

    js wip

---
+++ src/bda/plone/checkout/browser/checkout.js
@@ -0,0 +1,20 @@
+(function($) {
+    
+    $(document).ready(function() {
+        var delivery_address = $('div.delivery_address');
+        var toggle = function(input) {
+            if (input.attr('checked') == true) {
+                delivery_address.show();
+            } else {
+                delivery_address.hide();
+            }
+        }
+        var sel = 'input[name=checkout.delivery_address.alternative_delivery]';
+        var input = $(sel);
+        toggle(input);
+        input.change(function(event) {
+            toggle($(this));
+        });
+    });
+
+})(jQuery);
