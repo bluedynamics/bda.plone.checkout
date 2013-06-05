bd7096fa659c8299f96f9bc96ccac9a7d28bda39
Robert Niederreiter <office@squarewave.at>
2013-06-05 13:12:17 +0200

    housekeeping

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
-from setuptools import setup, find_packages
 import os
+from setuptools import (
+    setup,
+    find_packages,
+)
+
 
 version = '1.0dev'
 shortdesc = "Checkout"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
+
 
 setup(name='bda.plone.checkout',
       version=version,
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
-            'Environment :: Web Environment',
-            'License :: OSI Approved :: GNU General Public License (GPL)',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Environment :: Web Environment',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
       ],
       author='BlueDynamics Alliance',
       author_email='dev@bluedynamics.com',
       license='GNU General Public Licence',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['bda', 'bda.plone'],
       include_package_data=True,
       zip_safe=False,
@@ -35,4 +42,4 @@ setup(name='bda.plone.checkout',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-)
+      )
