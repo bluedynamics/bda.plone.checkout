75ad652edbccd38b99d2480b73613986a586bf99
Peter Holzer <peter.holzer@agitator.com>
2019-02-06 21:04:40 +0100

    remove p4 overlay

--- src/bda/plone/checkout/browser/checkout.js
+++ src/bda/plone/checkout/browser/checkout.js
@@ -23,14 +23,6 @@
             toggle($(this));
         });
 
-        // terms and conditions overlay in P4.
-        if ($('body').data('pat-plone-model') !== undefined) {
-            $('a.terms_and_conditions').prepOverlay({
-                subtype: 'ajax',
-                filter: common_content_filter,
-                cssclass: 'overlay-terms-and-condition'
-            });
-        }
     });
 
 }(jQuery));
